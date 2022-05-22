8f1f7b27ffb93a145138fd3b1a8040d60261acc2
Tapas <tapasmahanandia@gmail.com>
2022-05-23 00:08:59 +0530

    first commit

---
+++ FIRST/Basics/myfirstcontract.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier:UNLICENSED
+//@title Tapas My first solidty contract 
+
+/*
+basic syntax of solidity
+
+contract <contract_name>{
+
+    constructor() <visibility>{
+        .......
+    }
+    // rest code
+}
+
+*/
+pragma solidity 0.8.14; // version of solidiyt
+contract myfirst{  
+    // basic contract formation 
+}
--- FIRST/myfirstcontract.sol
+++
@@ -1,7 +0,0 @@
-//SPDX-License-Identifier:UNLICENSED
-//@title Tapas My first solidty contract 
-
-pragma solidity 0.8.14;
-contract myfirst{
-    // body of  the contract is done
-}
