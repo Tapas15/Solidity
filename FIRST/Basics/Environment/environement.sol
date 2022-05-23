cd167440583224b22064046ed4478f547868dd4c
Tapas <tapasmahanandia@gmail.com>
2022-05-23 05:30:08 +0530

    environment

---
+++ FIRST/Basics/Environment/environement.sol
@@ -0,0 +1,27 @@
+/* 
+This is the basic environment setup for solidity app developmet 
+in computer 
+
+solidity tools for windows 
+
+install visual studio code 
+install ganache 
+install truffle 
+install solidity
+install ethereum
+a youtube link is best describes is provided below 
+please follow the link below for deteail instuction to install
+https://youtu.be/ijqfiO-c3ng
+https://youtu.be/SnGwaRZ1Ci0
+
+install visuaal studio code in computer 
+do it in a particular locatoin 
+install node js 
+remix project installation beneth the ide works as solidity component like remix online ide to work as offline 
+
+npm install @remix-project/remix-solidity
+install truffle npm install truffle -g 
+check in terminal truffle version 
+and ready to go 
+
+*/
