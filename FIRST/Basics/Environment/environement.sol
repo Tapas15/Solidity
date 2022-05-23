6bd41f6020fc44db7d66b534ab22445d1ffff5d7
Tapas <tapasmahanandia@gmail.com>
2022-05-23 09:02:51 +0530

    added

--- FIRST/Basics/Environment/environement.sol
+++ FIRST/Basics/Environment/environement.sol
@@ -11,22 +11,41 @@ install solidity
 install ethereum
 install extension for visual studio code Tailwind CSS IntelliSense
 install solidity extension Juan Blanco
+install remix extension vs code 
 a youtube link is best describes is provided below 
 please follow the link below for deteail instuction to install
 https://youtu.be/ijqfiO-c3ng
 https://youtu.be/SnGwaRZ1Ci0
+https://youtu.be/nQsyLa3djeU
+conect your remix with local for testing and deployemnt of smart contract 
+https://youtu.be/LgLkAjc3UtA
+connect git online ide 
+https://youtu.be/9zZ5mk74n3E
 
-install visuaal studio code in computer 
+
+install visuaal studio code in computer
+install hardhar in folder 
+npm install --save-dev hardhat
+install remix ide desktop failed to install
+git link failed 
+https://github.com/ethereum/remix-desktop/releases?page=1
+
+https://remix-ide.readthedocs.io/en/latest/ documantation learning resources 
+connect your online ide to the local-space on your computer to deploy contract and test on real network
 do it in a particular locatoin 
 install node js 
 remix project installation beneth the ide works as solidity component like remix online ide to work as offline 
 
 npm install @remix-project/remix-solidity
 install truffle npm install truffle -g 
+npm install --save-dev hardhat
 check in terminal truffle version 
 npm truffle init // find if this works
 truffle console 
-turffle init // initializaiton token successfull
+turffle init // initializaiton token succesfull ready now
 and ready to go 
 
 */
+
+
+
