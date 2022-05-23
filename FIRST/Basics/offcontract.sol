7d711609743ec3a39b7249cabbb9d2889ead43e2
Tapas <tapasmahanandia@gmail.com>
2022-05-23 16:50:23 +0530

    add files

--- FIRST/Basics/offcontract.sol
+++ FIRST/Basics/offcontract.sol
@@ -3,11 +3,19 @@
 pragma solidity 0.8.14;
 contract first_basic{
 //This is our first smart contract
-string private name; // sting variable can be private or public 
+string private name; // sting specify variable type , private is access specifier mode ,name suggest variable name 
+ uint private age;
 
-function setName(string memory _name) public {
-    name = _name;
+// function name is set to go as input function
+function setName(string memory _name) public {// function is keyword , setName is function name ,(its argument area)Public is type of view private or public , and returns a value
+        name = _name;// variable inside function , we call it as input setName function 
+        age = 18; // age
+  }
+// here the calling funtion to return name and age of person 
 
+function getName() public view returns(string memory,uint)  // string memory pe return ho raha he
+{
+    return(name,age); // this function will retur name and age 
 }
 
 
