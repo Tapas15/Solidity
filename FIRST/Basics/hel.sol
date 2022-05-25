550d67cda2f7376104506b0300db9f840e058eda
Tapas <tapasmahanandia@gmail.com>
2022-05-26 01:36:18 +0530

    changes made

---
+++ FIRST/Basics/hel.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:UNLICENSED
+/// @title Hello-world contract 
+/// @author The name of the author Tapas 
+/// @notice It gives hellow world
+/// @dev it will return hellow world
+ pragma solidity 0.8.14;
+contract Helloworld {
+     function setc() public pure returns(string memory)
+     {
+         return 'Hellow world';
+     }
+
+}
+
